fde8a287f94be1457fda80aee4809a7298929e3c
percobaantutu <restumuhammad@upi.edu>
2025-01-07 07:12:46 +0700

    Making Cardproduct.header and Cardproduct.body inside the CardProduct component, so it's reusable

--- src/Components/Fragments/CardProduct.jsx
+++ src/Components/Fragments/CardProduct.jsx
@@ -1,18 +1,36 @@
-const CardProduct = () => {
+import { Link } from "react-router-dom";
+
+const CardProduct = ({ children }) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
+    <Link to="/">
+      {" "}
+      <div className="card bg-base-100 w-56 shadow-xl mx-3">{children}</div>
+    </Link>
+  );
+};
+
+const Header = () => {
+  return (
+    <figure>
+      <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+    </figure>
+  );
+};
+
+const Body = (props) => {
+  const { productname, children } = props;
+  return (
+    <div className="card-body">
+      <h2 className="card-title">{productname}</h2>
+      <p>{children}</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary">Buy Now</button>
       </div>
     </div>
   );
 };
+
+CardProduct.Header = Header;
+CardProduct.Body = Body;
 
 export default CardProduct;
--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -3,7 +3,18 @@ import CardProduct from "../Components/Fragments/CardProduct";
 const Products = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <CardProduct></CardProduct>
+      <CardProduct>
+        <CardProduct.Header />
+        <CardProduct.Body productname="Shoes">If a dog chews shoes whose shoes does he choose? </CardProduct.Body>
+      </CardProduct>
+      <CardProduct>
+        <CardProduct.Header />
+        <CardProduct.Body productname="Shoes">If a dog chews shoes whose shoes does he choose? </CardProduct.Body>
+      </CardProduct>
+      <CardProduct>
+        <CardProduct.Header />
+        <CardProduct.Body productname="Shoes">If a dog chews shoes whose shoes does he choose? </CardProduct.Body>
+      </CardProduct>
     </div>
   );
 };
