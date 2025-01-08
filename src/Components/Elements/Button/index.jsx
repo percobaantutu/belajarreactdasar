9a515177b28773304ecb4b28418cc76df6b5de4a
percobaantutu <restumuhammad@upi.edu>
2025-01-08 22:10:34 +0700

    Made login system using local storage

--- src/Components/Elements/Button/index.jsx
+++ src/Components/Elements/Button/index.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
-  const { children = "...", variant = "bg-black" } = props;
+  const { children = "...", variant = "bg-black", onClick = () => {}, type = "button" } = props;
   return (
-    <button className={`h-10 px-6 font-semibold rounded-md ${variant} w-full`} type="submit">
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} w-full`} type={type} onClick={() => onClick()}>
       {children}
     </button>
   );
--- src/Components/Fragments/FormLogin.jsx
+++ src/Components/Fragments/FormLogin.jsx
@@ -2,11 +2,19 @@ import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
 
 const FormLogin = () => {
+  const handleLogin = (event) => {
+    localStorage.setItem(`email`, event.target.email.value);
+    localStorage.setItem(`password`, event.target.password.value);
+    event.preventDefault();
+    window.location.href = "/products";
+  };
   return (
-    <form action="">
+    <form onSubmit={handleLogin}>
       <InputForm title="Email" name="email" placeholder="example@gmail.com" type="email" />
       <InputForm title="Password" name="password" placeholder="*****" type="password" />
-      <Button variant="bg-blue-600 text-white">Submit</Button>
+      <Button variant="bg-blue-600 text-white" type="submit">
+        Submit
+      </Button>
     </form>
   );
 };
